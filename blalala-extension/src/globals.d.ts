d36a7c0d5ed9330bb30bd8ab914982439ecf1088
Yves Guillo <yvesguillo@gmail.com>
2026-06-14 20:40:25 +0200

    added some atributes in persistan classes to prevent new process amnesy.

--- blalala-extension/src/globals.d.ts
+++ blalala-extension/src/globals.d.ts
@@ -1,3 +1,5 @@
+// This script is implicitly loaded by the extension manager and holds global aliases and classes.
+
 interface BlalalaParams {
     persona: string;
     tone: string;
@@ -5,10 +7,17 @@ interface BlalalaParams {
     customInstruction: string;
 }
 
+interface BlalalaTextNode {
+    node: Text;
+    originalText: string;
+}
+
 interface BlalalaContentApi {
     getTextNodes(root?: Node): Text[];
     wrappTextNodes(nodes?: Text[]): BlalalaTextNode[];
     transformPage(params: BlalalaParams): Promise<void>;
+    textNodes: Text[] | undefined;
+    wrappedTextNodes: BlalalaTextNode[] | undefined;
 }
 
 interface Window {
